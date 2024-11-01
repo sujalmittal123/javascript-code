93caf89b19f1202ebdfdce16b6aa25b5635419f9
sujalmittal123 <148077550+sujalmittal123@users.noreply.github.com>
2024-11-01 21:44:11 +0530

    new

---
+++ 01basics/Str_to_num.js
@@ -0,0 +1,11 @@
+let number1 = 24
+console.log(number1)
+
+let number2 = "24"
+console.log(number2)
+
+console.log(typeof(number1, number2))
+
+let valueInNumber = Number(number2)
+console.log(valueInNumber)
+console.log(typeof(valueInNumber))
