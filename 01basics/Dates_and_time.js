4b552f4a088a3104f4bc920010a69df58698b785
sujalmittal123 <148077550+sujalmittal123@users.noreply.github.com>
2024-11-09 19:17:21 +0530

    basic date and time concept

---
+++ 01basics/Dates_and_time.js
@@ -0,0 +1,13 @@
+////// dates ///////////////////////
+
+let myDate = new Date()
+console.log(myDate)
+console.log(myDate.getTime())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+console.log(typeof myDate)
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
+console.log(typeof myTimeStamp)
